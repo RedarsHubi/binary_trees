3b4459a8b897c14d5c21d0e3d71b1d2a7dfc7ba9
root <redarshubi@github.com>
2024-01-04 10:28:20 -0800

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t depth = 0;
+	size_t left, right;
 
 	if (tree == NULL)
 		return (0);
-       	while (tree->parent != NULL)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-	return depth;
+	
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	
+	return (left > right) ? (left + 1) : (right + 1);
 }
