26194997448e8c5187eeeecd25e45424bc51d2b8
root <redarshubi@github.com>
2024-01-04 10:10:00 -0800

    9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+/**
+ * binary_tree_height - measures height of tree
+ * @tree: root
+ * Return: height tree or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+       	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return depth;
+}
