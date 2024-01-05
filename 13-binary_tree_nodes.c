9484fd44ba8826bfe726d86b4ae82b7851fbaa2c
root <redarshubi@github.com>
2024-01-05 11:00:00 -0800

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,8 +14,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	
 
-	count = binary_tree_nodes(tree->right);
-	count = binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+	count += binary_tree_nodes(tree->left);
 
 	return (count);
 }
